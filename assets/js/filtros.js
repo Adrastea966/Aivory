49cf85c70863bddc5dc871a80b291db5634c6a61
Adrastea <Adrastea966@yahoo.com>
2023-08-17 02:37:06 -0300

    Se agregó la funcionalidad 'filtros'

---
+++ assets/js/filtros.js
@@ -0,0 +1,24 @@
+//Array de objetos
+document.addEventListener('DOMContentLoaded', function () {
+    const arrayDeObjetos = [
+        { nombre: 'Desarrollo web', id: 1, categoria: 'Trabajo' },
+        { nombre: 'Diseño grafico', id: 2, categoria: 'Trabajo' },
+        { nombre: 'Desarrollo web', id: 3, categoria: 'Curiosidad' },
+        { nombre: 'Chatsonic', id: 4, categoria: 'Ocio' },
+        { nombre: 'Writings', id: 5, categoria: 'Curiosidad' }
+    ];
+
+    const verTrabajosButton = document.querySelector('.ver-trabajos');
+
+    verTrabajosButton.addEventListener('click', function () {
+        const categoriaTrabajo = arrayDeObjetos.find(objeto => objeto.categoria === 'Trabajo');
+
+        if (categoriaTrabajo) {
+            const trabajos = arrayDeObjetos.filter(objeto => objeto.categoria === 'Trabajo');
+            console.log(trabajos);
+        } else {
+            console.log("No se encontraron AI con esa categoría");
+        }
+    });
+});
+
--- assets/js/index.js
+++ assets/js/index.js
@@ -12,32 +12,34 @@ $(function () {
 });
 
 //Seccion AI - dialogos
-let bloqueSecundario = document.querySelector('.container-dialogos-secundarios');
-let bloqueSecundarioDos = document.querySelector('.container-dialogos-secundarios-2');
-let bloqueSecundarioTres = document.querySelector('.container-dialogos-secundarios-3');
-let bloquePrimario = document.querySelector('.container-dialogos-principales');
-let decisiones = document.querySelector('.decisiones');
-let curiosidad = document.querySelector('.curiosidad');
-let trabajo = document.querySelector('.trabajo');
-let ocio = document.querySelector('.ocio');
+document.addEventListener('DOMContentLoaded', function () {
+  let bloqueSecundario = document.querySelector('.container-dialogos-secundarios');
+  let bloqueSecundarioDos = document.querySelector('.container-dialogos-secundarios-2');
+  let bloqueSecundarioTres = document.querySelector('.container-dialogos-secundarios-3');
+  let bloquePrimario = document.querySelector('.container-dialogos-principales');
+  let decisiones = document.querySelector('.decisiones');
+  let curiosidad = document.querySelector('.curiosidad');
+  let trabajo = document.querySelector('.trabajo');
+  let ocio = document.querySelector('.ocio');
 
-trabajo.addEventListener('click', function (event) {
-  event.preventDefault();
-  bloqueSecundario.classList.remove('display-none');
-  bloquePrimario.classList.add('display-none');
-  decisiones.classList.add('display-none');
-});
+  trabajo.addEventListener('click', function (event) {
+      event.preventDefault();
+      bloqueSecundario.classList.remove('display-none');
+      bloquePrimario.classList.add('display-none');
+      decisiones.classList.add('display-none');
+  });
 
-ocio.addEventListener('click', function (event) {
-  event.preventDefault();
-  bloqueSecundarioDos.classList.remove('display-none');
-  bloquePrimario.classList.add('display-none');
-  decisiones.classList.add('display-none');
-});
+  ocio.addEventListener('click', function (event) {
+      event.preventDefault();
+      bloqueSecundarioDos.classList.remove('display-none');
+      bloquePrimario.classList.add('display-none');
+      decisiones.classList.add('display-none');
+  });
 
-curiosidad.addEventListener('click', function (event) {
-  event.preventDefault();
-  bloqueSecundarioTres.classList.remove('display-none');
-  bloquePrimario.classList.add('display-none');
-  decisiones.classList.add('display-none');
+  curiosidad.addEventListener('click', function (event) {
+      event.preventDefault();
+      bloqueSecundarioTres.classList.remove('display-none');
+      bloquePrimario.classList.add('display-none');
+      decisiones.classList.add('display-none');
+  });
 });
